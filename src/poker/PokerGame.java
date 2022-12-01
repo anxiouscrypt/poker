e583fba9da2fee94985b6fffa18f6e96484ee854
anxiouscrypt <anxiouscrypt@gmail.com>
2022-11-30 23:06:50 -0500

    when player folds next round is still in game

--- src/poker/PokerGame.java
+++ src/poker/PokerGame.java
@@ -22,6 +22,9 @@ public class PokerGame {
 		this.keepPlaying = true;
 		playerSetup();
 		while (this.keepPlaying) {
+			for (int p = 0; p < this.playerList.size(); p++) {
+				this.playerList.get(p).setIsInGame(true);
+			}
 			// First round of betting (pre-flop)
 			bettingRound();
 			System.out.println("1");
@@ -186,67 +189,89 @@ public class PokerGame {
 		 *  Switch if statments to switch case
 		 */
 		ArrayList<Player> winningPlayers = new ArrayList<Player>();
+		
+		
 		//royalFlush
 		winningPlayers = royalFlush();
 		if (winningPlayers.size() > 0) {
 			handleWinners(winningPlayers);
+			System.out.println("Royal Flush");
+			System.out.println("Winner(s) are: " + winningPlayers);
 			return;
 		}
 		//straightFlush
 		winningPlayers = straightFlush();
 		if (winningPlayers.size() > 0) {
 			handleWinners(winningPlayers);
+			System.out.println("Straight Flush");
+			System.out.println("Winner(s) are: " + winningPlayers);
 			return;
 		}
 		//fourOfAKind
 		winningPlayers = fourOfAKind();
 		if (winningPlayers.size() > 0) {
 			handleWinners(winningPlayers);
+			System.out.println("Four of a Kind");
+			System.out.println("Winner(s) are: " + winningPlayers);
 			return;
 		}
 		//fullHouse
 		winningPlayers = fullHouse();
 		if (winningPlayers.size() > 0) {
 			handleWinners(winningPlayers);
+			System.out.println("Full House");
+			System.out.println("Winner(s) are: " + winningPlayers);
 			return;
 		}
 		//flush
 		winningPlayers = flush();
 		if (winningPlayers.size() > 0) {
 			handleWinners(winningPlayers);
+			System.out.println("Flush");
+			System.out.println("Winner(s) are: " + winningPlayers);
 			return;
 		}
 		//straight
 		winningPlayers = straight();
 		if (winningPlayers.size() > 0) {
 			handleWinners(winningPlayers);
+			System.out.println("Straight");
+			System.out.println("Winner(s) are: " + winningPlayers);
 			return;
 		}
 		//threeOfAKind
 		winningPlayers = threeOfAKind();
 		if (winningPlayers.size() > 0) {
 			handleWinners(winningPlayers);
+			System.out.println("Three of a Kind");
+			System.out.println("Winner(s) are: " + winningPlayers);
 			return;
 		}
 		//twoPair
 		winningPlayers = twoPair();
 		if (winningPlayers.size() > 0) {
 			handleWinners(winningPlayers);
+			System.out.println("Two Pair");
+			System.out.println("Winner(s) are: " + winningPlayers);
 			return;
 		}
 		//onePair
 		winningPlayers = onePair();
 		if (winningPlayers.size() > 0) {
 			handleWinners(winningPlayers);
+			System.out.println("One Pair");
+			System.out.println("Winner(s) are: " + winningPlayers);
 			return;
 		}
 		//highCard
 		winningPlayers = highCard();
 		if (winningPlayers.size() > 0) {
 			handleWinners(winningPlayers);
+			System.out.println("High Card");
+			System.out.println("Winner(s) are: " + winningPlayers);
 			return;
 		}
-		System.out.println("Winner(s) are: " + winningPlayers);
+		
 	}
 	
 	public void handleWinners(ArrayList<Player> winningPlayers) {
